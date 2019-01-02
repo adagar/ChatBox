eaf1d01cc9a12fbd75467e0007a473002a28ff50
adagar <adagar121987@gmail.com>
2019-01-02 15:13:48 -0500

    Breaking up into component files

---
+++ chat-box/src/containers/AddMessage.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class AddMessage extends Component {
+    render(){
+        return (
+            <section id="new-message">New message</section>
+        );
+    }
+}
+
+export default AddMessage;
---
+++ chat-box/src/containers/MessagesList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class MessagesList extends Component {
+    render(){
+        return (
+            <section id="messages-list">Messages list</section>
+        )
+    }
+}
+
+export default MessagesList;
